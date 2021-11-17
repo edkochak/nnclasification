e9e3a2a77175d893aa2da8dc4e49c639bd17a51e
edkochak <edcay97@gmail.com>
2021-11-18 00:38:22 +0300

    New models and img from internet

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import cv2
 import torchvision.transforms as T
 from torchvision import models
 import torchvision
-import os
+import os,sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -16,17 +16,22 @@ from torchsummary import summary
 from torch.utils.tensorboard import SummaryWriter
 
 
+
+
+
+
 def get_num_correct(preds, labels):
     # Количество правильных предиктов
     return preds.argmax(dim=1).eq(labels).sum().item()
 
 
-def get_data_loader(folder='train', batch_size=6):
+def get_data_loader(folder='train', batch_size=32):
     # Создаем датасет
     data_dir = os.path.join(os.getcwd(), folder)
     transform = T.Compose([
         T.Resize([47, 47]),
         T.RandomHorizontalFlip(),
+        T.RandomRotation(degrees=(-45, 45)),
         T.ToTensor()
     ])
 
@@ -42,9 +47,6 @@ class Net(nn.Module):
         super().__init__()
         self.model_name = 'resnet18'
         self.model = models.resnet18(pretrained=True)
-        # for param in self.model.parameters():
-        #     # замораживаем слои, которые уже натренированы
-        #     param.requires_grad = False
 
         self.model.fc = nn.Sequential(nn.Linear(
             self.model.fc.in_features, 3000), nn.ReLU(), nn.Linear(3000, num_classes))
@@ -55,18 +57,22 @@ class Net(nn.Module):
         return x
 
 
-def train_model(x, names):
+def train_model( names):
     tb = SummaryWriter()
     net = Net(len(names))
     net.train()
+    NEPOCH=40
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-    for epoch in range(20):
+    
+    for epoch in range(NEPOCH):
+        x = get_data_loader('train')
         total_loss = 0
         total_correct = 0
+        if epoch==int(NEPOCH/2):
+            for param in list(net.model.parameters())[:-4]:
+                param.requires_grad = False
         for imgs_batch, answers_batch in x:
-
             out = net(imgs_batch)
             optimizer.zero_grad()
             loss = criterion(out, answers_batch)
@@ -78,47 +84,70 @@ def train_model(x, names):
         tb.add_scalar("Loss", total_loss, epoch)
         tb.add_scalar("Correct", total_correct, epoch)
         tb.add_scalar("Accuracy", total_correct / len(x.dataset), epoch)
+        tb.add_scalar("Check on test", raw_check(model=net), epoch)
 
     tb.close()
     return net
 
 
-def main():
-    names = [
-        'George_W_Bush', 'Colin_Powell', 'Tony_Blair', 'Donald_Rumsfeld', 'Gerhard_Schroeder', 'Ariel_Sharon', 'Hugo_Chavez', 'Junichiro_Koizumi', 'Serena_Williams', 'John_Ashcroft', 'Jacques_Chirac', 'Vladimir_Putin'
-    ]
-    data = get_data_loader('train')
-    nn = train_model(data, names)
-
-    torch.save(nn.state_dict(), '3.model')
+def main_for_train():
+    nn = train_model(names)
+    torch.save(nn.state_dict(), '5.model')
 
 
-def check():
-    names = [
-        'George_W_Bush', 'Colin_Powell', 'Tony_Blair', 'Donald_Rumsfeld', 'Gerhard_Schroeder', 'Ariel_Sharon', 'Hugo_Chavez', 'Junichiro_Koizumi', 'Serena_Williams', 'John_Ashcroft', 'Jacques_Chirac', 'Vladimir_Putin'
-    ]
+def raw_check(model=None):
+    if model==None:
+        model = Net(len(names))
+        model.load_state_dict(torch.load('5.model'))
+    model.eval()
+    n=9
+    total_correct = 0
+    for _ in range(n):
+        tests = get_data_loader('test/', 1)
+        for imgs_batch, answers_batch in tests:
+            out = model(imgs_batch)
+            check_predicts = get_num_correct(out, answers_batch)
+            total_correct += check_predicts
+    model.train()
+    return total_correct/(len(tests.dataset)*n)
+
+    
+
+
+def check_for_one(path):
+    transform = T.Compose([
+        T.Resize([47, 47]),
+        T.ToTensor()
+    ])
     model = Net(len(names))
-    model.load_state_dict(torch.load('3.model'))
+    model.load_state_dict(torch.load('5.model'))
     model.eval()
 
-    tests = get_data_loader('test/', 1)
-    total_correct = 0
-    for imgs_batch, answers_batch in tests:
-        out = model(imgs_batch)
-        check_predicts = get_num_correct(out, answers_batch)
+    img = transform(Image.open(path)).unsqueeze(0)
 
-        if check_predicts != len(answers_batch):
-            # Вывод неправильного предикта
-            ind_incorrect = out.argmax(dim=1).eq(
-                answers_batch).tolist().index(False)
-            print(f"Оценка нейронки - {out[ind_incorrect].tolist()}")
-            print(f'Правильный ответ: {answers_batch[ind_incorrect].item()}')
+    predict = model(img)
+    
+    answer= predict.argmax(dim=1)
+    print(names[answer.item()])
 
-        total_correct += check_predicts
-
-    print(total_correct/len(tests.dataset))
-
+def get_names():
+    data = get_data_loader('train')
+    names={}
+    for path,index in data.dataset.samples:
+        if index not in names:
+            name = path.split('train\\')[-1].split('\\')[0]
+            names[index]=name
+    return names
+    
 
 if __name__ == '__main__':
-    # main()
-    check()
+    names= get_names()
+    print(names)
+    # main_for_train()
+    print(f'Проверка на тестах:{raw_check()}')
+    print('Фотографии из интернета:')
+    check_for_one(r'img_from_internet\vladimir.jpg')
+    check_for_one(r'img_from_internet\serena.jpg')
+    check_for_one(r'img_from_internet\Bush_George_W_HS.jpg')
+    check_for_one(r'img_from_internet\chirac.jpg')
+    
