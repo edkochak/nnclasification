6f18c9d49913adcb98b3e381a94d9b104c0b5b0e
edkochak <edcay97@gmail.com>
2021-11-19 01:32:37 +0300

    new model, viola jones method

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import cv2
 import torchvision.transforms as T
 from torchvision import models
 import torchvision
-import os,sys
+import os
+import sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -16,22 +17,18 @@ from torchsummary import summary
 from torch.utils.tensorboard import SummaryWriter
 
 
-
-
-
-
 def get_num_correct(preds, labels):
     # Количество правильных предиктов
     return preds.argmax(dim=1).eq(labels).sum().item()
 
 
-def get_data_loader(folder='train', batch_size=32):
+def get_data_loader(folder='train', batch_size=16):
     # Создаем датасет
     data_dir = os.path.join(os.getcwd(), folder)
     transform = T.Compose([
         T.Resize([47, 47]),
         T.RandomHorizontalFlip(),
-        T.RandomRotation(degrees=(-45, 45)),
+        T.RandomRotation(degrees=(-20, 20)),
         T.ToTensor()
     ])
 
@@ -57,19 +54,19 @@ class Net(nn.Module):
         return x
 
 
-def train_model( names):
+def train_model(names):
     tb = SummaryWriter()
     net = Net(len(names))
     net.train()
-    NEPOCH=40
+    NEPOCH = 30
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    
+
     for epoch in range(NEPOCH):
-        x = get_data_loader('train')
+        x = get_data_loader(r'C:\nn\viola-jones\onlyfaces1')
         total_loss = 0
         total_correct = 0
-        if epoch==int(NEPOCH/2):
+        if epoch == int(NEPOCH/2):
             for param in list(net.model.parameters())[:-4]:
                 param.requires_grad = False
         for imgs_batch, answers_batch in x:
@@ -92,26 +89,24 @@ def train_model( names):
 
 def main_for_train():
     nn = train_model(names)
-    torch.save(nn.state_dict(), '5.model')
+    torch.save(nn.state_dict(), '6.model')
 
 
 def raw_check(model=None):
-    if model==None:
+    if model == None:
         model = Net(len(names))
-        model.load_state_dict(torch.load('5.model'))
+        model.load_state_dict(torch.load('6.model'))
     model.eval()
-    n=9
+    n = 9
     total_correct = 0
     for _ in range(n):
-        tests = get_data_loader('test/', 1)
+        tests = get_data_loader(r'C:\nn\viola-jones\onlyfacestest1', 1)
         for imgs_batch, answers_batch in tests:
             out = model(imgs_batch)
             check_predicts = get_num_correct(out, answers_batch)
             total_correct += check_predicts
     model.train()
     return total_correct/(len(tests.dataset)*n)
-
-    
 
 
 def check_for_one(path):
@@ -120,34 +115,46 @@ def check_for_one(path):
         T.ToTensor()
     ])
     model = Net(len(names))
-    model.load_state_dict(torch.load('5.model'))
+    model.load_state_dict(torch.load('6.model'))
     model.eval()
 
-    img = transform(Image.open(path)).unsqueeze(0)
+    original_image = cv2.imread(path)
+    grayscale_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml')
+    detected_faces = face_cascade.detectMultiScale(grayscale_image)
+    if detected_faces == ():
+        return 'Нет лиц'
+    (column, row, width, height) = detected_faces[0]
+    d=30
+    original_image = original_image[row-d:row +height+d, column-d:column+width+d]
+
+            
+    img =Image.fromarray(original_image[:,:,::-1])
+    img = transform(img).unsqueeze(0)
 
     predict = model(img)
-    
-    answer= predict.argmax(dim=1)
-    print(names[answer.item()])
+
+    answer = predict.argmax(dim=1)
+    # print([(item,names[i]) for i,item in enumerate(predict.tolist()[0])])
+    return names[answer.item()]
+
 
 def get_names():
     data = get_data_loader('train')
-    names={}
-    for path,index in data.dataset.samples:
+    names = {}
+    for path, index in data.dataset.samples:
         if index not in names:
             name = path.split('train\\')[-1].split('\\')[0]
-            names[index]=name
+            names[index] = name
     return names
-    
+
 
 if __name__ == '__main__':
-    names= get_names()
+    names = get_names()
     print(names)
     # main_for_train()
     print(f'Проверка на тестах:{raw_check()}')
     print('Фотографии из интернета:')
-    check_for_one(r'img_from_internet\vladimir.jpg')
-    check_for_one(r'img_from_internet\serena.jpg')
-    check_for_one(r'img_from_internet\Bush_George_W_HS.jpg')
-    check_for_one(r'img_from_internet\chirac.jpg')
-    
+    for file in os.listdir('img_from_internet'):
+        print(check_for_one("img_from_internet/"+file),file)
